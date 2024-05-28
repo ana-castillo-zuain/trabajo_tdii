d6ba710c56e119879b550952710e4937f40fb5f6
ana-castillo-zuain <164687068+ana-castillo-zuain@users.noreply.github.com>
2024-05-28 20:10:57 -0300

    arreglo error models
    añadir max lenght

--- datos/sitio/models.py
+++ datos/sitio/models.py
@@ -3,7 +3,7 @@ from django import forms
 # Create your models here.
 class Schools(models.Model):
     DBN = models.CharField(max_length=200)
-    School_Name = models.CharField(default=0) 
+    School_Name = models.CharField(default=0, max_length=200) 
     Number_of_Test_Takers = models.IntegerField(default=0) 
     Critical_Reading_Mean = models.IntegerField(default=0) 
     Mathematics_Mean = models.IntegerField(default=0) 
