d4f4c5a6a5418ba4f968ddad30e687e394766ce6
ana-castillo-zuain <164687068+ana-castillo-zuain@users.noreply.github.com>
2024-06-01 20:04:10 -0300

    creacion de views para la visualizacion de datos

--- datos/sitio/views.py
+++ datos/sitio/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.templatetags.static import static
 from django.contrib.staticfiles.storage import staticfiles_storage
-
+from django.db.models import F, ExpressionWrapper, FloatField
 from .models import School
 import csv
+import matplotlib.pyplot as plt 
 
 # Create your views here.
 def index(request):
@@ -28,4 +29,57 @@ def upload_file(request):
 
 def view_data(request):
     data = School.objects.all()
+    # School.objects.annotate(
+    #     Average_Score=ExpressionWrapper(
+    #         (F('Critical_Reading_Mean')+F('Mathematics_Mean')+F('Writing_Mean'))
+    #         /3.00, output_field=FloatField()
+    #         )
+    #     )
+    # names = [data.School_Name for data in data]
+    # students = [data.Number_of_Test_Takers for data in data]
+    # reading = [data.Critical_Reading_Mean for data in data]
+    # maths = [data.Mathematics_Mean for data in data]
+    # writing = [data.Writing_Mean for data in data]
+    # avg = [data.Average_Score for data in data]
+    sample_schools = data[:10]
+    top10_maths = data.order_by('-Mathematics_Mean')[:10]
+    school_names = [school.School_Name for school in top10_maths]
+    math_means = [school.Mathematics_Mean for school in top10_maths]
+    plt.figure(figsize=(10, 6))
+    plt.barh(school_names, math_means, color='crimson')
+    plt.xlabel('Mathematics Mean Score')
+    plt.title('Top 10 Schools by Mathematics Mean Score')
+    plt.gca().invert_yaxis()
+    plt.savefig('top10maths.png')
+
+    top10_reading = data.order_by('-Critical_Reading_Mean')[:10]
+    school_namesm = [school.School_Name for school in top10_reading]
+    math_means = [school.Critical_Reading_Mean for school in top10_reading]
+    plt.figure(figsize=(10, 6))
+    plt.barh(school_namesm, math_means, color='lighseagreen')
+    plt.xlabel('Critical Reading Mean Score')
+    plt.title('Top 10 Schools by Critical Reading Mean Score')
+    plt.gca().invert_yaxis()
+    plt.savefig('top10reading.png')
+
+    top10_reading = data.order_by('-Critical_Reading_Mean')[:10]
+    school_namesr = [school.School_Name for school in top10_reading]
+    reading_means = [school.Critical_Reading_Mean for school in top10_reading]
+    plt.figure(figsize=(10, 6))
+    plt.barh(school_namesr, reading_means, color='lighseagreen')
+    plt.xlabel('Critical Reading Mean Score')
+    plt.title('Top 10 Schools by Critical Reading Mean Score')
+    plt.gca().invert_yaxis()
+    plt.savefig('top10reading.png')
+
+    top10_writing = data.order_by('-Writing_Means')[:10]
+    school_namesw = [school.School_Name for school in top10_writing]
+    writing_means = [school.Critical_Reading_Mean for school in top10_writing]
+    plt.figure(figsize=(10, 6))
+    plt.barh(school_namesw, writing_means, color='skyblue')
+    plt.xlabel('Critical Reading Mean Score')
+    plt.title('Top 10 Schools by Critical Reading Mean Score')
+    plt.gca().invert_yaxis()
+    plt.savefig('top10reading.png')
+
     return render(request, 'data.html', {'data': data })
