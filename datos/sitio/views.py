7119fe9780ac55db49797b4b614dab6b464019b9
ana-castillo-zuain <164687068+ana-castillo-zuain@users.noreply.github.com>
2024-06-01 15:54:50 -0300

    pasaje de datos de listas a models

--- datos/sitio/views.py
+++ datos/sitio/views.py
@@ -14,21 +14,18 @@ def upload_file(request):
     with open (staticfiles_storage.path('dataset.csv'), 'r') as file:
         reader = csv.reader(file)
         reader.__next__()
-        rowaux = []
         for row in reader:
-            print(row)
-            rowaux = row
-            # School.objects.create(
-            #     DBN = row[0],
-            #     School_Name = row[1],
-            #     Number_of_Test_Takers = row[2],
-            #     Critical_Reading_Mean = row[3],
-            #     Mathematics_Mean = row[4],
-            #     Writing_Mean = row[5],
-            #     )
-    # return HttpResponseRedirect('data/')
-    return render(rowaux)
+            School.objects.create(
+                 DBN = row[0],
+                 School_Name = row[1],
+                 Number_of_Test_Takers = row[2],
+                 Critical_Reading_Mean = row[3],
+                 Mathematics_Mean = row[4],
+                 Writing_Mean = row[5],
+                 )
+    return HttpResponseRedirect('data/')
+
 
 def view_data(request):
     data = School.objects.all()
-    return render(request, 'data.html', {'data':data})
+    return render(request, 'data.html', {'data': data })
