9f82bfb51cee493b61c4f2c5efc40e1989853d0a
ana-castillo-zuain <164687068+ana-castillo-zuain@users.noreply.github.com>
2024-06-03 20:16:43 -0300

    estadisiticas y top 10 avg

--- datos/sitio/views.py
+++ datos/sitio/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.conf import settings
 from .models import School
+from django.db.models import Avg, Max, Min
 import csv
 import matplotlib.pyplot as plt 
 import os
@@ -46,12 +47,42 @@ def view_data(request):
     static_dir = os.path.join(settings.STATICFILES_DIRS[0], 'images')
     if not os.path.exists(static_dir):
         os.makedirs(static_dir)
+    
+    top10_average = data.order_by('-Average_Scores')[:10]
+    school_nameso = [school.School_Name for school in top10_average]
+    average = [school.Average_Score for school in top10_average]
+    plt.figure(figsize=(10, 6))
+    plt.barh(school_nameso, average, color='orange')
+    plt.xlabel('Average Score')
+    plt.title('Top 10 Schools by Average Score')
+    plt.gca().invert_yaxis()
+    average_image_path = os.path.join(static_dir, 'top10average.png')
+    plt.savefig(average_image_path)
+    plt.close()
+
+    statistics = {
+        'Critical Reading': {
+            'average': School.objects.all().aggregate(Avg('Critical_Reading_Mean'))['Critical_Reading_Mean__avg'],
+            'max': School.objects.all().aggregate(Max('Critical_Reading_Mean'))['Critical_Reading_Mean__max'],
+            'min': School.objects.all().aggregate(Min('Critical_Reading_Mean'))['Critical_Reading_Mean__min'],
+        },
+        'Mathematics': {
+            'average': School.objects.all().aggregate(Avg('Mathematics_Mean'))['Mathematics_Mean__avg'],
+            'max': School.objects.all().aggregate(Max('Mathematics_Mean'))['Mathematics_Mean__max'],
+            'min': School.objects.all().aggregate(Min('Mathematics_Mean'))['Mathematics_Mean__min'],
+        },
+        'Writing': {
+            'average': School.objects.all().aggregate(Avg('Writing_Mean'))['Writing_Mean__avg'],
+            'max': School.objects.all().aggregate(Max('Writing_Mean'))['Writing_Mean__max'],
+            'min': School.objects.all().aggregate(Min('Writing_Mean'))['Writing_Mean__min'],
+        }
+    }
 
     top10_maths = data.order_by('-Mathematics_Mean')[:10]
     school_names = [school.School_Name for school in top10_maths]
     math_means = [school.Mathematics_Mean for school in top10_maths]
     plt.figure(figsize=(10, 6))
-    plt.barh(school_names, math_means, color='crimson')
+    plt.barh(school_names, math_means, color='maroon')
     plt.xlabel('Mathematics Mean Score')
     plt.title('Top 10 Schools by Mathematics Mean Score')
     plt.gca().invert_yaxis()
@@ -63,7 +94,7 @@ def view_data(request):
     school_namesm = [school.School_Name for school in top10_reading]
     math_means = [school.Critical_Reading_Mean for school in top10_reading]
     plt.figure(figsize=(10, 6))
-    plt.barh(school_namesm, math_means, color='crimson')
+    plt.barh(school_namesm, math_means, color='seagreen')
     plt.xlabel('Critical Reading Mean Score')
     plt.title('Top 10 Schools by Critical Reading Mean Score')
     plt.gca().invert_yaxis()
@@ -73,7 +104,7 @@ def view_data(request):
 
     top10_writing = data.order_by('-Writing_Means')[:10]
     school_namesw = [school.School_Name for school in top10_writing]
-    writing_means = [school.Critical_Reading_Mean for school in top10_writing]
+    writing_means = [school.Writing_Mean for school in top10_writing]
     plt.figure(figsize=(10, 6))
     plt.barh(school_namesw, writing_means, color='skyblue')
     plt.xlabel('Critical Reading Mean Score')
@@ -85,9 +116,11 @@ def view_data(request):
 
     context = {
         'data': data,
+        'statistics': statistics,
         'math_image': 'images/top10maths.png',
         'reading_image': 'images/top10reading.png',
-        'writing_image': 'images/top10writing.png'
+        'writing_image': 'images/top10writing.png',
+        'average_image': 'images/top10average.png'
     }
 
-    return render(request, 'data.html',context)
+    return render(request, 'data.html', context)
